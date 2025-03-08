2c1f6bef66044e882dee505e32da997166e0efcb
Jean Piere <javierjeanpieres@gmail.com>
2025-03-08 18:54:13 -0500

    refactor - remove UserController and request classes; clean up routes

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//route login
-Route::get('login', function () {
-    return view('login');
-})->name('login');
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('users', [UserController::class, 'index'])->name('users.index');
-    Route::get('users/create', [UserController::class, 'create'])->name('users.create');
-    Route::get('users/{id}', [UserController::class, 'show'])->name('users.show');
-    Route::post('users', [UserController::class, 'store'])->name('users.store');
-    Route::get('users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-    Route::put('users/{id}', [UserController::class, 'update'])->name('users.update');
-    Route::delete('users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-});
 
 
